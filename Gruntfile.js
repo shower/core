7be4561d2cb5ab66fbf9ac03da598252f3bb1851
Denis Hananein <i@zloylos.me>
2014-09-07 17:54:42 +0400

    auto initialization & get plugins from attr data-plugins

--- Gruntfile.js
+++ Gruntfile.js
@@ -14,6 +14,7 @@ module.exports = function(grunt) {
             dist: {
                 src: [
                     'node_modules/ym/modules.js',
+                    'lib/init.js',
                     'lib/*/*.js',
                     'plugins/*.js'
                 ],
