ac3755547ed7efda6a1b180bf695179513ea7911
Denis Hananein <i@zloylos.me>
2014-12-30 19:47:45 +0300

    unit test for the Shower core class

--- tests/unit/test.Shower.js
+++ tests/unit/test.Shower.js
@@ -1,12 +1,47 @@
 modules.define('test.Shower', [
-    'shower' 
-], function (provide, shower) {
-    
+    'shower',
+    'Slide'
+], function (provide, shower, Slide) {
+
     var should = chai.should();
 
     describe('Shower', function () {
-        it('Should be ready', function () { 
+        it('Should be ready', function () {
             shower.ready().should.eq(true);
+        });
+
+        it('Should add slide to the Shower', function () {
+            var slide = new Slide('test slide');
+            shower.add(slide);
+
+            shower.getSlidesArray().indexOf(slide).should.not.eq(-1);
+        });
+
+        it('Should remove slide from the Shower', function () {
+            var slide = new Slide('test slide');
+            shower.add(slide);
+            shower.getSlidesArray().indexOf(slide).should.not.eq(-1);
+
+            shower.remove(slide);
+            shower.getSlidesArray().indexOf(slide).should.eq(-1);
+        });
+
+        it('Should get slide by the index', function () {
+            var slide = new Slide('test slide'),
+                slidesCount = shower.getSlidesCount();
+
+            shower.add(slide);
+
+            // slidesCount == last index + 1
+            shower.get(slidesCount).should.eq(slide);
+        });
+
+        it('Should change count of slides after add', function () {
+            var slide = new Slide('test slide'),
+                oldCount = shower.getSlidesCount();
+
+            shower.add(slide);
+            shower.getSlidesCount().should.eq(oldCount + 1);
         });
     });
 
