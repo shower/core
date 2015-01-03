001754ec6050424184e49fdc005586870d03d950
Denis Hananein <i@zloylos.me>
2015-01-03 22:56:10 +0300

    unit-tests for Player and container

---
+++ tests/unit/test.shower.Container.js
@@ -0,0 +1,13 @@
+modules.define('test.shower.Container', [
+    'shower',
+    'Slide'
+], function (provide, shower, Slide) {
+
+    var should = chai.should();
+
+    describe('shower.Container', function () {
+        it('Shoult tur ')
+    });
+
+    provide();
+});
