d97f54239e12e267ab3ad03f9b19bef0cb08512c
Denis Hananein <i@zloylos.me>
2015-01-03 23:49:51 +0300

    unit tests for shower.Container

--- tests/unit/test.shower.Container.js
+++ tests/unit/test.shower.Container.js
@@ -1,12 +1,58 @@
 modules.define('test.shower.Container', [
     'shower',
-    'Slide'
-], function (provide, shower, Slide) {
+    'shower.Container'
+], function (provide, shower, Container) {
 
     var should = chai.should();
 
     describe('shower.Container', function () {
-        it('Shoult tur ')
+    	var containerElement,
+    		container; 
+
+    	before(function () {
+    		containerElement = document.createElement('div');
+    		containerElement.id = 'shower-test-container';
+
+    		document.body.appendChild(containerElement);
+    	});
+
+    	after(function () {
+    		containerElement.parentNode
+    			.removeChild(containerElement);
+    	});
+
+    	beforeEach(function () {
+    		container = new Container(shower, containerElement);
+    	});
+
+    	afterEach(function () {
+    		container.destroy();
+    	});
+
+        it('Should turn to the slide mode', function () {
+        	container.enterSlideMode();
+        	document.body
+        		.classList.contains('shower--full')
+        		.should.eq(true);
+        });
+
+        it('Should turn to the list mode', function () {
+			var bodyClassList = document.body.classList;
+        	
+        	container
+        		.enterSlideMode()
+        		.exitSlideMode();
+
+        	bodyClassList.contains('shower--full')
+        		.should.eq(false);
+
+        	bodyClassList.contains('shower--list')
+        		.should.eq(true);
+        });	
+
+        it('Should return the container element', function () {
+        	container.getElement().should.eq(containerElement);
+        });
     });
 
     provide();
