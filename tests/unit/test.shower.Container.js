f638395e6eb54d2d0972f58d45af40c5d5b4a850
Denis Hananein <i@zloylos.me>
2015-01-03 23:54:47 +0300

    translate tabs to spaces

--- tests/unit/test.shower.Container.js
+++ tests/unit/test.shower.Container.js
@@ -6,52 +6,52 @@ modules.define('test.shower.Container', [
     var should = chai.should();
 
     describe('shower.Container', function () {
-    	var containerElement,
-    		container; 
+        var containerElement,
+            container; 
 
-    	before(function () {
-    		containerElement = document.createElement('div');
-    		containerElement.id = 'shower-test-container';
+        before(function () {
+            containerElement = document.createElement('div');
+            containerElement.id = 'shower-test-container';
 
-    		document.body.appendChild(containerElement);
-    	});
+            document.body.appendChild(containerElement);
+        });
 
-    	after(function () {
-    		containerElement.parentNode
-    			.removeChild(containerElement);
-    	});
+        after(function () {
+            containerElement.parentNode
+                .removeChild(containerElement);
+        });
 
-    	beforeEach(function () {
-    		container = new Container(shower, containerElement);
-    	});
+        beforeEach(function () {
+            container = new Container(shower, containerElement);
+        });
 
-    	afterEach(function () {
-    		container.destroy();
-    	});
+        afterEach(function () {
+            container.destroy();
+        });
 
         it('Should turn to the slide mode', function () {
-        	container.enterSlideMode();
-        	document.body
-        		.classList.contains('shower--full')
-        		.should.eq(true);
+            container.enterSlideMode();
+            document.body
+                .classList.contains('shower--full')
+                .should.eq(true);
         });
 
         it('Should turn to the list mode', function () {
-			var bodyClassList = document.body.classList;
-        	
-        	container
-        		.enterSlideMode()
-        		.exitSlideMode();
+            var bodyClassList = document.body.classList;
+            
+            container
+                .enterSlideMode()
+                .exitSlideMode();
 
-        	bodyClassList.contains('shower--full')
-        		.should.eq(false);
+            bodyClassList.contains('shower--full')
+                .should.eq(false);
 
-        	bodyClassList.contains('shower--list')
-        		.should.eq(true);
-        });	
+            bodyClassList.contains('shower--list')
+                .should.eq(true);
+        });    
 
         it('Should return the container element', function () {
-        	container.getElement().should.eq(containerElement);
+            container.getElement().should.eq(containerElement);
         });
     });
 
