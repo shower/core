9ed1592f34491ce270cc65b0b90dfd157a056f17
Aleksey Shvayka <shvaikalesh@gmail.com>
2017-03-02 04:36:39 +0200

    remove deep nesting for func tests

--- tests/func/keys.js
+++ tests/func/keys.js
@@ -5,70 +5,70 @@ module.exports = {
     // Forward
 
     'activates the first slide in `list` mode if Right Arrow key is pressed': browser => {
-        browser.url(`file:///${__dirname}/list.html`);
+        browser.url(`${browser.launchUrl}/list.html`);
         browser.keys(browser.Keys.ARROW_RIGHT);
         browser.assert.cssClassPresent('[id="1"]', 'active');
         browser.end();
     },
 
     'moves forward when Right Arrow key is pressed': browser => {
-        browser.url(`file:///${__dirname}/list.html#1`);
+        browser.url(`${browser.launchUrl}/list.html#1`);
         browser.keys(browser.Keys.ARROW_RIGHT);
         browser.assert.cssClassPresent('[id="2"]', 'active');
         browser.end();
     },
 
     'moves forward when Down Arrow key is pressed': browser => {
-        browser.url(`file:///${__dirname}/list.html#1`);
+        browser.url(`${browser.launchUrl}/list.html#1`);
         browser.keys(browser.Keys.ARROW_DOWN);
         browser.assert.cssClassPresent('[id="2"]', 'active');
         browser.end();
     },
 
     'moves forward when Page Down key is pressed': browser => {
-        browser.url(`file:///${__dirname}/list.html#1`);
+        browser.url(`${browser.launchUrl}/list.html#1`);
         browser.keys(browser.Keys.PAGEDOWN);
         browser.assert.cssClassPresent('[id="2"]', 'active');
         browser.end();
     },
 
     'moves forward when J key is pressed': browser => {
-        browser.url(`file:///${__dirname}/list.html#1`);
+        browser.url(`${browser.launchUrl}/list.html#1`);
         browser.keys('J');
         browser.assert.cssClassPresent('[id="2"]', 'active');
         browser.end();
     },
 
     'moves forward when L key is pressed': browser => {
-        browser.url(`file:///${__dirname}/list.html#1`);
+        browser.url(`${browser.launchUrl}/list.html#1`);
         browser.keys('L');
         browser.assert.cssClassPresent('[id="2"]', 'active');
         browser.end();
     },
 
     'moves forward when N key is pressed': browser => {
-        browser.url(`file:///${__dirname}/list.html#1`);
+        browser.url(`${browser.launchUrl}/list.html#1`);
         browser.keys('N');
         browser.assert.cssClassPresent('[id="2"]', 'active');
         browser.end();
     },
 
     'moves forward when Enter key is pressed': browser => {
-        browser.url(`file:///${__dirname}/list.html#1`);
+        browser.url(`${browser.launchUrl}/list.html#1`);
         browser.keys(browser.Keys.ENTER);
         browser.assert.cssClassPresent('[id="2"]', 'active');
         browser.end();
     },
 
     'moves forward when Space key is pressed in `full` mode': browser => {
-        browser.url(`file:///${__dirname}/full.html#1`);
+        browser.url(`${browser.launchUrl}/full.html#1`);
         browser.keys(browser.Keys.SPACE);
         browser.assert.cssClassPresent('[id="2"]', 'active');
         browser.end();
     },
 
     'doesn’t move forward when Space key is pressed in `list` mode': browser => {
-        browser.url(`file:///${__dirname}/list.html#1`);
+        browser.url(`${browser.launchUrl}/list.html#1`);
         browser.keys(browser.Keys.SPACE);
         browser.assert.cssClassPresent('[id="1"]', 'active');
         browser.end();
@@ -77,84 +77,84 @@ module.exports = {
     // Backward
 
     'moves backward when Left Arrow key is pressed': browser => {
-        browser.url(`file:///${__dirname}/list.html#2`);
+        browser.url(`${browser.launchUrl}/list.html#2`);
         browser.keys(browser.Keys.ARROW_LEFT);
         browser.assert.cssClassPresent('[id="1"]', 'active');
         browser.end();
     },
 
     'moves backward when Up Arrow key is pressed': browser => {
-        browser.url(`file:///${__dirname}/list.html#2`);
+        browser.url(`${browser.launchUrl}/list.html#2`);
         browser.keys(browser.Keys.ARROW_UP);
         browser.assert.cssClassPresent('[id="1"]', 'active');
         browser.end();
     },
 
     'moves backward when Page Up key is pressed': browser => {
-        browser.url(`file:///${__dirname}/list.html#2`);
+        browser.url(`${browser.launchUrl}/list.html#2`);
         browser.keys(browser.Keys.PAGEUP);
         browser.assert.cssClassPresent('[id="1"]', 'active');
         browser.end();
     },
 
     'moves backward when K key is pressed': browser => {
-        browser.url(`file:///${__dirname}/list.html#2`);
+        browser.url(`${browser.launchUrl}/list.html#2`);
         browser.keys('K');
         browser.assert.cssClassPresent('[id="1"]', 'active');
         browser.end();
     },
 
     'moves backward when H key is pressed': browser => {
-        browser.url(`file:///${__dirname}/list.html#2`);
+        browser.url(`${browser.launchUrl}/list.html#2`);
         browser.keys('H');
         browser.assert.cssClassPresent('[id="1"]', 'active');
         browser.end();
     },
 
     'moves backward when P key is pressed': browser => {
-        browser.url(`file:///${__dirname}/list.html#2`);
+        browser.url(`${browser.launchUrl}/list.html#2`);
         browser.keys('P');
         browser.assert.cssClassPresent('[id="1"]', 'active');
         browser.end();
     },
 
     'moves backward when Shift Enter keys are pressed in `list` mode': browser => {
-        browser.url(`file:///${__dirname}/list.html#2`);
+        browser.url(`${browser.launchUrl}/list.html#2`);
         browser.keys([browser.Keys.SHIFT, browser.Keys.ENTER]);
         browser.assert.cssClassPresent('[id="1"]', 'active');
         browser.end();
     },
 
     'moves backward when Shift Enter keys are pressed in `full` mode': browser => {
-        browser.url(`file:///${__dirname}/full.html#2`);
+        browser.url(`${browser.launchUrl}/full.html#2`);
         browser.keys([browser.Keys.SHIFT, browser.Keys.ENTER]);
         browser.assert.cssClassPresent('[id="1"]', 'active');
         browser.end();
     },
 
     'doesn’t move backward when Shift Space keys are pressed in `list` mode': browser => {
-        browser.url(`file:///${__dirname}/list.html#2`);
+        browser.url(`${browser.launchUrl}/list.html#2`);
         browser.keys([browser.Keys.SHIFT, browser.Keys.SPACE]);
         browser.assert.cssClassPresent('[id="2"]', 'active');
         browser.end();
     },
 
     'moves backward when Shift Space keys are pressed in `full` mode': browser => {
-        browser.url(`file:///${__dirname}/full.html#2`);
+        browser.url(`${browser.launchUrl}/full.html#2`);
         browser.keys([browser.Keys.SHIFT, browser.Keys.SPACE]);
         browser.assert.cssClassPresent('[id="1"]', 'active');
         browser.end();
     },
 
     'goes to first slide when Home key is pressed': browser => {
-        browser.url(`file:///${__dirname}/list.html#3`);
+        browser.url(`${browser.launchUrl}/list.html#3`);
         browser.keys(browser.Keys.HOME);
         browser.assert.cssClassPresent('[id="1"]', 'active');
         browser.end();
     },
 
     'goes to last slide when End key is pressed': browser => {
-        browser.url(`file:///${__dirname}/list.html#1`);
+        browser.url(`${browser.launchUrl}/list.html#1`);
         browser.keys(browser.Keys.END);
         browser.assert.cssClassPresent('[id="3"]', 'active');
         browser.end();
@@ -163,7 +163,7 @@ module.exports = {
     // Start End
 
     'starts presenation from the current slide when Shift F5 keys are pressed': browser => {
-        browser.url(`file:///${__dirname}/list.html#2`);
+        browser.url(`${browser.launchUrl}/list.html#2`);
         browser.keys([browser.Keys.SHIFT, browser.Keys.F5]);
         browser.assert.cssClassPresent('[id="2"]', 'active');
         browser.assert.elementPresent('.shower.full');
@@ -171,7 +171,7 @@ module.exports = {
     },
 
     'starts presenation from the first slide when Cmd Shift Enter keys are pressed': browser => {
-        browser.url(`file:///${__dirname}/list.html#2`);
+        browser.url(`${browser.launchUrl}/list.html#2`);
         browser.keys([browser.Keys.META, browser.Keys.SHIFT, browser.Keys.ENTER]);
         browser.assert.cssClassPresent('[id="1"]', 'active');
         browser.assert.elementPresent('.shower.full');
@@ -179,7 +179,7 @@ module.exports = {
     },
 
     'starts presenation from the current slide when Cmd Enter keys are pressed': browser => {
-        browser.url(`file:///${__dirname}/list.html#2`);
+        browser.url(`${browser.launchUrl}/list.html#2`);
         browser.keys([browser.Keys.META, browser.Keys.ENTER]);
         browser.assert.cssClassPresent('[id="2"]', 'active');
         browser.assert.elementPresent('.shower.full');
@@ -187,7 +187,7 @@ module.exports = {
     },
 
     'stops presenation when Esc key is pressed': browser => {
-        browser.url(`file:///${__dirname}/full.html#1`);
+        browser.url(`${browser.launchUrl}/full.html#1`);
         browser.keys(browser.Keys.ESCAPE);
         browser.assert.elementPresent('.shower.list');
         browser.end();
