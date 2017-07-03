3d6cb1800fa8cefb0934650c543a263c1dd03ec6
Aleksey Shvayka <shvaikalesh@gmail.com>
2017-07-03 23:29:45 +0300

    use rollup

---
+++ rollup.config.js
@@ -0,0 +1,24 @@
+'use strict';
+const resolve = require('rollup-plugin-node-resolve');
+
+const pkg = require('./package.json');
+const now = new Date().getFullYear();
+const banner = `
+/**
+ * ${pkg.description}
+ * ${pkg.name} v${pkg.version}, ${pkg.homepage}
+ * @copyright 2010-${now} ${pkg.author.name}, ${pkg.author.url}
+ * @license ${pkg.license}
+ */
+`;
+
+module.exports = {
+    entry: 'lib/index.js',
+    plugins: [
+        resolve(),
+    ],
+    format: 'iife',
+    moduleName: 'shower',
+    banner,
+    dest: 'dist/shower.js',
+};
