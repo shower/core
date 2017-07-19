39f8d93ab2f12d4cbcac85c264be1d6ab8d05597
Aleksey Shvayka <shvaikalesh@gmail.com>
2017-07-20 00:30:15 +0300

    improve rollup banner

--- rollup.config.js
+++ rollup.config.js
@@ -1,15 +1,14 @@
 'use strict';
 
 const resolve = require('rollup-plugin-node-resolve');
-
 const pkg = require('./package.json');
 
+const ndash = '\u2013';
 const now = new Date().getFullYear();
-const banner = `
-/**
+const banner = `/**
  * ${pkg.description}
  * ${pkg.name} v${pkg.version}, ${pkg.homepage}
- * @copyright 2010-${now} ${pkg.author.name}, ${pkg.author.url}
+ * @copyright 2010${ndash}${now} ${pkg.author.name}, ${pkg.author.url}
  * @license ${pkg.license}
  */
 `;
