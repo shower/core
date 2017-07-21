f62ad72c437fdd4e7e9de2d5a725ee46a865dff4
Aleksey Shvayka <shvaikalesh@gmail.com>
2017-07-21 03:11:03 +0300

    improve & reorder core tests

--- test/func/core.js
+++ test/func/core.js
@@ -21,15 +21,23 @@ module.exports = {
         browser.end();
     },
 
+    'goes to `full` mode when a slide is clicked': browser => {
+        browser.url(`${browser.launchUrl}/list.html`);
+        browser.click('[id="1"]');
+        browser.assert.elementPresent('.shower.full');
+        browser.end();
+    },
+
     'keeps `full` mode after reload': browser => {
-        browser.url(`${browser.launchUrl}/full.html`);
+        browser.url(`${browser.launchUrl}/list.html`);
+        browser.click('[id="3"]');
         browser.refresh();
         browser.assert.elementPresent('.shower.full');
         browser.end();
     },
 
     'adds IDs to all slides unless alredy set': browser => {
-        browser.url(`${browser.launchUrl}/id.html`);
+        browser.url(`${browser.launchUrl}/list-id.html`);
         browser.assert.elementPresent('[id="1"]');
         browser.assert.elementNotPresent('[id="2"]');
         browser.assert.elementPresent('#id');
@@ -45,7 +53,7 @@ module.exports = {
     },
 
     'sets `active` state to a current slide only (not `visited`)': browser => {
-        browser.url(`${browser.launchUrl}/id.html#id`);
+        browser.url(`${browser.launchUrl}/list-id.html#id`);
         browser.assert.cssClassNotPresent('[id="1"]', 'active');
         browser.assert.cssClassNotPresent('[id="1"]', 'visited');
         browser.assert.cssClassPresent('#id', 'active');
@@ -54,16 +62,6 @@ module.exports = {
         browser.assert.cssClassNotPresent('[id="3"]', 'visited');
         browser.end();
     },
-
-    // Action
-
-    'goes to `full` mode when a slide is clicked': browser => {
-        browser.url(`${browser.launchUrl}/list.html`);
-        browser.click('[id="3"]');
-        browser.assert.elementPresent('.shower.full');
-        browser.end();
-    },
-
     // Traverse
 
     'changes slides states when moving forward': browser => {
