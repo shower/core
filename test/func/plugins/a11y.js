6c9ca76c1f7cd07a348da2e4bda02efe278a30b2
Aleksey Shvayka <shvaikalesh@gmail.com>
2017-12-17 22:46:29 +0200

    Add live region tests

--- test/func/plugins/a11y.js
+++ test/func/plugins/a11y.js
@@ -28,4 +28,56 @@ module.exports = {
         browser.assert.elementPresent('.shower[role=application]');
         browser.end();
     },
+
+    'initially sets live region in `list` mode': browser => {
+        browser.url(`${browser.launchUrl}/list.html#3`);
+        browser.assert.containsText('.region h2', '3');
+        browser.end();
+    },
+
+    'initially sets live region in `full` mode': browser => {
+        browser.url(`${browser.launchUrl}/full.html#2`);
+        browser.assert.containsText('.region h2', '2');
+        browser.end();
+    },
+
+    'updates live region when moving forwards in `list` mode': browser => {
+        browser.url(`${browser.launchUrl}/list.html#1`);
+        browser.assert.containsText('.region h2', '1');
+        browser.sendKeys('.send-keys', browser.Keys.ARROW_RIGHT);
+        browser.assert.containsText('.region h2', '2');
+        browser.sendKeys('.send-keys', browser.Keys.ARROW_RIGHT);
+        browser.assert.containsText('.region h2', '3');
+        browser.end();
+    },
+
+    'updates live region when moving backwards in `list` mode': browser => {
+        browser.url(`${browser.launchUrl}/list.html#3`);
+        browser.assert.containsText('.region h2', '3');
+        browser.sendKeys('.send-keys', browser.Keys.ARROW_LEFT);
+        browser.assert.containsText('.region h2', '2');
+        browser.sendKeys('.send-keys', browser.Keys.ARROW_LEFT);
+        browser.assert.containsText('.region h2', '1');
+        browser.end();
+    },
+
+    'updates live region when moving forwards in `full` mode': browser => {
+        browser.url(`${browser.launchUrl}/full.html#1`);
+        browser.assert.containsText('.region h2', '1');
+        browser.sendKeys('.send-keys', browser.Keys.ARROW_RIGHT);
+        browser.assert.containsText('.region h2', '2');
+        browser.sendKeys('.send-keys', browser.Keys.ARROW_RIGHT);
+        browser.assert.containsText('.region h2', '3');
+        browser.end();
+    },
+
+    'updates live region when moving backwards in `full` mode': browser => {
+        browser.url(`${browser.launchUrl}/full.html#3`);
+        browser.assert.containsText('.region h2', '3');
+        browser.sendKeys('.send-keys', browser.Keys.ARROW_LEFT);
+        browser.assert.containsText('.region h2', '2');
+        browser.sendKeys('.send-keys', browser.Keys.ARROW_LEFT);
+        browser.assert.containsText('.region h2', '1');
+        browser.end();
+    },
 };
