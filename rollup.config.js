bdffa980a9c6e84dc97f14da5af980966864a07b
Aleksey Shvayka <shvaikalesh@gmail.com>
2018-10-27 09:53:40 +0300

    Code tweaks

--- rollup.config.js
+++ rollup.config.js
@@ -1,28 +1,27 @@
 'use strict';
-
-const resolve = require('rollup-plugin-node-resolve');
 const pkg = require('./package.json');
+const resolve = require('rollup-plugin-node-resolve');
+const commonjs = require('rollup-plugin-commonjs');
 
 const ndash = '\u2013';
 const now = new Date().getFullYear();
-const banner = `
-/**
+const banner = `/**
  * ${pkg.description}
  * ${pkg.name} v${pkg.version}, ${pkg.homepage}
  * @copyright 2010${ndash}${now} ${pkg.author.name}, ${pkg.author.url}
  * @license ${pkg.license}
- */
-`;
+ */`;
 
 module.exports = {
     input: 'lib/index.js',
-    plugins: [
-        resolve(),
-    ],
     output: {
         file: 'dist/shower.js',
         format: 'iife',
         name: 'shower',
         banner,
     },
+    plugins: [
+        resolve(),
+        commonjs(),
+    ],
 };
